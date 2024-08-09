5283fb4d6167666efc2ad0971e78531883b802f4
palakbansal8810 <140965358+palakbansal8810@users.noreply.github.com>
2024-08-09 21:34:13 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 ## END TO END PROJECT
+
+import dagshub
+dagshub.init(repo_owner='palakbansal8810', repo_name='DataScience-Project', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
+
+
+MLFLOW_TRACKING_URI=https://dagshub.com/palakbansal8810/DataScience-Project.mlflow
+MLFLOW_TRACKING_USERNAME=palakbansal8810
+MLFLOW_TRACKING_PASSWORD=7104284f1bb44ece21e0e2adb4e36a250ae3251f
+python script.py
--- app.py
+++ app.py
@@ -4,28 +4,32 @@ from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformationconfig,DataTransformation
 from src.mlproject.components.model_trainer import ModelTrainingConfig,ModelTrainer
+import dagshub
+import mlflow
+
 import sys
+dagshub.init(repo_owner='palakbansal8810', repo_name='DataScience-Project', mlflow=True)
+mlflow.set_tracking_uri("https://dagshub.com/palakbansal8810/DataScience-Project.mlflow")
 
+mlflow.set_experiment("Model Testing")
 
-if __name__=='__main__':
-    logging.info("The execution has started") 
+if __name__=="__main__":
+    logging.info("The execution has started")
 
     try:
-        # data_ingestion=DataIngestionConfig()
+        #data_ingestion_config=DataIngestionConfig()
         data_ingestion=DataIngestion()
         train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
 
-        # data_transformation_config=DataTransformationconfig()
-
+        #data_transformation_config=DataTransformationConfig()
         data_transformation=DataTransformation()
         train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
 
+        ## Model Training
+
         model_trainer=ModelTrainer()
         print(model_trainer.initiate_model_trainer(train_arr,test_arr))
-
-        
         
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
-    
