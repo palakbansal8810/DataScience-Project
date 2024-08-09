815ba237ca68c5bb27ccda691d760f1cbc5432bb
palakbansal8810 <140965358+palakbansal8810@users.noreply.github.com>
2024-08-09 16:11:09 +0530

    model training

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformationconfig,DataTransformation
+from src.mlproject.components.model_trainer import ModelTrainingConfig,ModelTrainer
 import sys
 
 
@@ -17,7 +18,10 @@ if __name__=='__main__':
         # data_transformation_config=DataTransformationconfig()
 
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
         
         
